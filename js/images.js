ac8075d9303b4b91060607000d0e59063bbe565f
Oleksandr Synook <ssineok21@gmail.com>
2023-05-15 16:39:56 +0300

    task2 bonus/counter fixed

--- js/images.js
+++ js/images.js
@@ -1,4 +1,5 @@
 const bigImages = document.querySelectorAll("img");
+let deletedImages = JSON.parse(localStorage.getItem("deletedImages")) || [];
 
 const overlayDiv = document.createElement("div");
 overlayDiv.classList.add("overlay");
@@ -31,3 +32,52 @@ bigImages.forEach((image) => {
     document.body.appendChild(overlayDiv);
   });
 });
+const deleteImage = (image) => {
+  deletedImages.push(image.src);
+  localStorage.setItem("deletedImages", JSON.stringify(deletedImages));
+  image.remove();
+  const images = document.querySelectorAll("img");
+  const imageCount = images.length;
+  infoDiv.innerText = `Кількість зображень на сторінці: ${imageCount} | ${new Date().toLocaleString(
+    "ua-UA"
+  )}`;
+};
+
+const restoreDeletedImages = () => {
+  deletedImages.forEach((deletedImage) => {
+    const images = document.querySelectorAll(`img[src="${deletedImage}"]`);
+    images.forEach((image) => {
+      image.style.display = "block";
+    });
+  });
+  deletedImages = [];
+  localStorage.removeItem("deletedImages");
+};
+
+bigImages.forEach((image) => {
+  const cross = document.createElement("span");
+  cross.innerHTML = "&#10060;";
+  cross.classList.add("cross");
+  const imageWrapper = document.createElement("div");
+  imageWrapper.classList.add("image-wrapper");
+  imageWrapper.appendChild(image);
+  imageWrapper.appendChild(cross);
+  cross.addEventListener("click", (event) => {
+    event.stopPropagation();
+    deleteImage(image);
+  });
+  document.querySelector(".grid").appendChild(imageWrapper);
+
+  if (deletedImages.includes(image.src)) {
+    image.style.display = "none";
+  }
+});
+
+const restoreButton = document.createElement("button");
+restoreButton.innerText = "Відновити";
+restoreButton.classList.add("restore-button");
+restoreButton.addEventListener("click", () => {
+  restoreDeletedImages();
+});
+
+document.body.appendChild(restoreButton);
